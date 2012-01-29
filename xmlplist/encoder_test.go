b4c1ea75db9abcbc6897fac8f79e1b2c2ae2b680
Mikkel Krautz <mikkel@krautz.dk>
2012-01-29 14:58:38 +0100

    Improved encoder tests.

--- xmlplist/encoder_test.go
+++ xmlplist/encoder_test.go
@@ -1,114 +1,83 @@
 package xmlplist
 
 import (
-	"log"
+	"bytes"
+	"io/ioutil"
 	"testing"
 	"time"
 )
 
-// todo(mkrautz): Implement proper tests, not just prints
-
-func TestIntEncode(t *testing.T) {
-	val := int64(42)
-	buf, err := Marshal(val)
-	if err != nil {
-		t.Fatalf("unable to marshal: %v", err)
-	}
-
-	log.Printf("%v", string(buf))
+type EncoderTest struct {
+	GoldenFile  string
+	Value       interface{}
 }
 
-func TestArrayEncode(t *testing.T) {
-	slice := []int64{1, 2, 3}
-	buf, err := Marshal(slice)
+func onceUponATime() time.Time {
+	t, err := time.Parse(time.RFC3339, "2012-01-29T13:07:25Z")
 	if err != nil {
-		t.Fatalf("unable to marshal: %v", err)
+		return t
 	}
-
-	log.Printf("%v", string(buf))
+	return t.UTC()
 }
 
-func TestByteSliceEncode(t *testing.T) {
-	bytes := []byte{0xff, 0xff, 0xff}
-	buf, err := Marshal(bytes)
-	if err != nil {
-		t.Fatalf("unable to marshal: %v", err)
-	}
-
-	log.Printf("%v", string(buf))
-}
-
-func TestFloatEncode(t *testing.T) {
-	f := float64(3.14159265)
-	buf, err := Marshal(f)
-	if err != nil {
-		t.Fatalf("unable to marshal: %v", err)
-	}
-
-	log.Printf("%v", string(buf))
-}
-
-func TestEncodeString(t *testing.T) {
-	str := "hey what < is up />"
-	buf, err := Marshal(str)
-	if err != nil {
-		t.Fatalf("unable to marshal: %v", err)
-	}
-
-	log.Printf("%v", string(buf))
-}
-
-func TestEncodeMap(t *testing.T) {
-	dict := map[string]interface{}{
-		"one": 1,
-		"two": 2,
-		"three": 3,
-		"bytes": []byte{0xff, 0xe3, 0xaf},
-	}
-	buf, err := Marshal(dict)
-	if err != nil {
-		t.Fatalf("unable to marshal: %v", err)
-	}
-
-	log.Printf("%v", string(buf))
-}
-
-func TestEncodeStruct(t *testing.T) {
-	e := Entitlements{
-		GetTaskAllow: true,
-	}
-	buf, err := Marshal(e)
-	if err != nil {
-		t.Fatalf("unable to marshal: %v", err)
-	}
-
-	log.Printf("%v", string(buf))
-}
-
-func TestEncodeRecursiveStruct(t *testing.T) {
-	re := RecursiveEntitlements{
-		GetTaskAllow: true,
-		Entitlements: Entitlements{
+var encTests []EncoderTest = []EncoderTest{
+	{
+		"testdata/Int.plist.golden",
+		int64(42),
+	},
+	{
+		"testdata/Array.plist.golden",
+		[]int64{1, 2, 3},
+	},
+	{
+		"testdata/Data.plist.golden",
+		[]byte{0xff, 0xff, 0xff},
+	},
+	{
+		"testdata/Float.plist.golden",
+		float64(3.14159265),
+	},
+	{
+		"testdata/String.plist.golden",
+		"hey what < is up />",
+	},
+	{
+		"testdata/Struct.plist.golden",
+		Entitlements{
 			GetTaskAllow: true,
 		},
-	}
-	buf, err := Marshal(re)
-	if err != nil {
-		t.Fatalf("unable to marshal: %v", err)
-	}
-
-	log.Printf("%v", string(buf))
+	},
+	{
+		"testdata/RecursiveStruct.plist.golden",
+		RecursiveEntitlements{
+			GetTaskAllow: true,
+			Entitlements: Entitlements{
+				GetTaskAllow: true,
+			},
+		},
+	},
+	{
+		"testdata/Date.plist.golden",
+		onceUponATime(),
+	},
 }
 
-func TestEncodeDate(t *testing.T) {
-	m := map[string]interface{}{
-		"now": time.Now(),
-	}
+func TestEncoder(t *testing.T) {
+	for _, test := range encTests {
+		buf, err := ioutil.ReadFile(test.GoldenFile)
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
 
-	buf, err := Marshal(m)
-	if err != nil {
-		t.Fatalf("unable to marshal: %v", err)
-	}
+		bw := new(bytes.Buffer)
+		e := NewEncoder(bw)
+		err = e.Encode(test.Value)
+		if err != nil {
+			t.Fatalf("%v", err)
+		}
 
-	log.Printf("%v", string(buf))
+		if !bytes.Equal(bw.Bytes(), buf) {
+			t.Fatalf("test mismatch for file: %v", test.GoldenFile)
+		}
+	}
 }
